4437ae73aeee15412d3acc478a51d22b7bd21db6
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-04-03 13:13:19 +0000

    fix

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,18 +8,18 @@
  * Return: pointer to the byte in s that matches one of the bytes in accept,
  * or NULL if no such byte is found
  */
-
 char *_strpbrk(char *s, char *accept)
 {
-	int i, j;
+	int i;
 
-	for (i = 0; s[i]; i++)
+	while (*s)
 	{
-		for (j = 0; accept[j]; j++)
+		for (i = 0; accept[i]; i++)
 		{
-			if (s[i] == accept[j])
-				return (&s[i]);
+		if (*s == accept[i])
+		return (s);
 		}
+	s++;
 	}
-	return (NULL);
+	return ('\0');
 }
