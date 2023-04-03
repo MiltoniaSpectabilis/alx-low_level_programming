62874ecee7cb24b30b844814fa75ea98bfbc6072
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-04-03 19:11:41 +0000

    set_string

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - sets the value of a pointer to a char
+ * @s: pointer to the pointer to set
+ * @to: the value to set it to
+ */
+
+void set_string(char **s, char *to)
+{
+    *s = to;
+}
