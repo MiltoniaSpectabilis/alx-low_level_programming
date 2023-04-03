8de55ce7451b827fea9fb99acdef55f382d51cbc
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-04-03 12:56:15 +0000

    fix

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,15 +7,14 @@
  *
  * Return: pointer to the first occurrence of c in s, or NULL if not found
  */
+
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-			return (s);
-		s++;
-	}
-	if (*s == c)
-		return (s);
-	return (NULL);
+    while (*s)
+    {
+        if (*s == c)
+            return (s);
+        s++;
+    }
+    return (NULL);
 }
