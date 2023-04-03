46cec93c7fa91dbaa7811e6b46450c1f63eb720a
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-04-03 18:57:20 +0000

    fix

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,13 +5,15 @@
  * @a: pointer to the chessboard
  */
 void print_chessboard(char (*a)[8])
-{
-    int i, j;
 
-    for (i = 0; i < 8; i++)
-    {
-        for (j = 0; j < 8; j++)
-            putchar(a[i][j]);
-        putchar('\n');
-    }
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+		_putchar('\n');
+	}
 }
