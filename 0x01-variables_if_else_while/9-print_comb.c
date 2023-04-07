48f69267af41bdf741628bda339a8328adcff134
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-04-07 08:24:07 +0000

    reset

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,29 @@
 #include <stdio.h>
 
+
 /**
- * main - Prints numbers between 0 to 9 with commas and spaces.
+ * main - prints all possible combinations of single-digit numbers.
  *
  * Return: Always 0 (Success)
  */
+
+
 int main(void)
 {
-	int i;
+	int sDigit = 48;
 
-	for (i = 48; i < 58; i++)
+	for (; sDigit <= 57; ++sDigit)
+
 	{
-		putchar(i);
-		if (i != 57)
-		{
+		putchar(sDigit);
+
+		if (sDigit != 57)
+
 			putchar(',');
-			putchar(' ');
-		}
+		putchar(' ');
 	}
+
 	putchar('\n');
+
 	return (0);
 }
