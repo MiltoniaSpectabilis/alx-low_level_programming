778d012ea910b0dc1c68a81e92bb3b070fdfd8b8
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-04-08 02:39:59 +0000

    reset

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * main - print _putchar
- * is written by mbah
- * Return: 0
+ * main - prints _putchar followed by a new line
+ * 
+ * Return: Always (0) (Success)
  */
 
 int main(void)
@@ -16,5 +16,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
