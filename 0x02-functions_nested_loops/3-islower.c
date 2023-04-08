421bb96e6efcfa7b37ab863c562828bc28947803
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-04-08 18:48:16 +0000

    reset

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,15 +1,23 @@
 #include "main.h"
 
 /**
- * _islower - check if char is lowercase
- * @c: is the char to be checked
- * Return: 1 if char is lowercase, otherwise 0.
+ * _islower - Checks for lowercase character.
+ *
+ * @c: random character
+ *
+ * Return:  (1) if character is lowercase, otherwise return (0)
  */
 
 int _islower(int c)
+
 {
 	if (c >= 'a' && c <= 'z')
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
+
 }
