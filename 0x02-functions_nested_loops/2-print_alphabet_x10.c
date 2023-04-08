1e2b4fbf1fd804d5237c9f31c1a6e234baae90c7
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-04-08 08:48:00 +0000

    reset

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
-* print_alphabet_x10 -  prints the alphabet ten times followed by a new line
-*/
+ * print_alphabet_x10 -  prints the alphabet ten times followed by a new line
+ */
 
- void print_alphabet_x10(void)
+void print_alphabet_x10(void)
 
 {
-    int i = 0;
-    char alphbt;
-    
-    while (i < 10)
-    {
-        for (alphbt = 'a'; alphbt <= 'z'; alphbt++)
-        {
-            _putchar(alphbt);
-        }
-        _putchar('\n');
-        i++;
-    }
-        
+	int i = 0;
+	char alphbt;
+
+	while (i < 10)
+	{
+		for (alphbt = 'a'; alphbt <= 'z'; alphbt++)
+		{
+			_putchar(alphbt);
+		}
+		_putchar('\n');
+		i++;
+	}
+
 }
