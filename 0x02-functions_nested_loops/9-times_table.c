3f3102e1147328e7137f502f0c4140e70eca2582
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-04-09 12:43:18 +0000

    reset

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,33 +5,33 @@
  */
 void times_table(void)
 {
-	int i, j, k;
+        int num1, num2, result;
 
-	for (i = 0; i < 10; i++)
-	{
-		for (j = 0; j < 10; j++)
-		{
-			k = j * i;
-			if (j == 0)
-			{
-				_putchar(k + '0');
-			}
-
-			if (k < 10 && j != 0)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
-			} else if (k >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
-			}
-		}
-		_putchar('\n');
-	}
+        for (num1 = 0; num1 <= 9; num1++)
+        {
+                for (num2 = 0; num2 <= 9; num2++)
+                {
+                        result = num1 * num2;
+                        if (result >= 10)
+                        {
+                                _putchar((result / 10) + '0');
+                                _putchar((result % 10) + '0');
+                        }
+                        else
+                        {
+                                _putchar(' ');
+                                _putchar(result + '0');
+                        }
+                        if (num2 == 9)
+                        {
+                                _putchar('\n');
+                        }
+                        else
+                        {
+                                _putchar(',');
+                                _putchar(' ');
+                        }
+                }
+        }
 }
 
