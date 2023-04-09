d4a405521b2c771e110cb80c43de431c17978812
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-04-09 07:45:42 +0000

    reset

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,8 +13,10 @@ void jack_bauer(void)
 	{
 		for (h = 0; h < 60; h++)
 		{
+			_putchar((m / 10) + '0');
 			_putchar((m % 10) + '0');
 			_putchar(':');
+			_putchar((h / 10) + '0');
 			_putchar((h % 10) + '0');
 			_putchar('\n');
 		}
