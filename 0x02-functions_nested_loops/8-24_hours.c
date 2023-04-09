8fae92c9df76195df61b60c0afb728a696feaf90
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-04-09 07:26:53 +0000

    reset

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,29 +1,20 @@
 #include "main.h"
 
 /**
- * jack_bauer - prints every minute of the day of Jack Bauer
- * starting from 00:00 to 23:59
+ * jack_bauer - prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ *
  */
+
 void jack_bauer(void)
 {
-	int i, j;
 
-	i = 0;
-
-	while (i < 24)
+	for (i = 0; i < 24; i++)
 	{
-		j = 0;
-		while (j < 60)
+		for (k = 0; k < 60; i++)
 		{
-			_putchar((i / 10) + '0');
-			_putchar((i % 10) + '0');
+			_putchar(i + '0');
 			_putchar(':');
-			_putchar((j / 10) + '0');
-			_putchar((j % 10) + '0');
-			_putchar('\n');
-			j++;
+			_putchar(k + '0');
 		}
-		i++;
 	}
 }
-
