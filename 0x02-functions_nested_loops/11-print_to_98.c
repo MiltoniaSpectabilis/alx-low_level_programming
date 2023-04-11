d4250820ae087a946acf7437b0e314786df4ca6e
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-04-11 07:28:49 +0000

    reset

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,25 +2,27 @@
 #include "main.h"
 
 /**
-*
-*
-*/
+ * print_to_98 - prints all natural numbers from n to 98
+ *
+ * @n: natural number
+ *
+ */
 
 void print_to_98(int n)
 {
-for (; n <= 98; n++)
-{
-	for (; n >= 98; n--)
+	for (; n <= 98; n++)
 	{
-		printf("%d, ", n);
-
-		if (n == 98)
+		for (; n >= 98; n--)
 		{
-			printf("%d", n);
+			printf("%d, ", n);
+
+			if (n == 98)
+			{
+				printf("%d", n);
+
+			}
 
 		}
-
 	}
-}
 
 }
