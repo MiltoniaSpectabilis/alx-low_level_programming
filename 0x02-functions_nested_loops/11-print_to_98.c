af2ad86494775c2461e99dba1fcb723b57e2cedd
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-04-11 09:43:54 +0000

    reset

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,32 +7,32 @@
  * @n: natural number
  *
  */
-void print_to_98(int n) 
+void print_to_98(int n)
 {
 
-	if (n <= 98) 
+	if (n <= 98)
 	{
-		for (; n <= 98; n++) 
+		for (; n <= 98; n++)
 		{
-			if (n != 98) 
+			if (n != 98)
 			{
 				printf("%d, ", n);
-			} 
-			else 
+			}
+			else
 			{
 				printf("%d\n", n);
 			}
 		}
-	} 
-	else 
+	}
+	else
 	{
-		for (; n >= 98; n--) 
+		for (; n >= 98; n--)
 		{
-			if (n != 98) 
+			if (n != 98)
 			{
 				printf("%d, ", n);
 			}
-			else 
+			else
 			{
 				printf("%d\n", n);
 			}
