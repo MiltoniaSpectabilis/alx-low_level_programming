91753a6ce048091e4d585e864fcee160c97ba9a7
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-04-13 05:33:28 +0000

    reset

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * _isdigit - check if numbers are 0 - 9
- * @c: char to be checked
- *
- * Return: 0 or 1
+ * _isdigit - checks for a digit (0 through 9)
+ * @c: random digit
+ * Return: 1 if c is a digit, otherwise 0
  */
 
 int _isdigit(int c)
+
 {
-	if (c >= '0' && c <= '9')
+	if (c >= 48 && c <= 57)
 		return (1);
 	else
 		return (0);
-}
 
+}
