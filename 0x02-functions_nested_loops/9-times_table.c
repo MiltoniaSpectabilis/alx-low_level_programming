5d7ffcc96ba478d7762e0f13995aa0d54dd153a0
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-04-14 11:01:05 +0000

    reset

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,21 +4,21 @@
  * times_table - prints the 9 times table, starting with 0
  */
 
-void times_table(void) 
+void times_table(void)
 {
 	int num1, num2, prod;
 
-	for (num1 = 0; num1 <= 9; num1++) 
+	for (num1 = 0; num1 <= 9; num1++)
 	{
-		for (num2 = 0; num2 <= 9; num2++) 
+		for (num2 = 0; num2 <= 9; num2++)
 		{
 			prod = num1 * num2;
 
-			if (num2 == 0) 
+			if (num2 == 0)
 			{
 				_putchar(prod + '0');
 			}
-			else if (num2 != 0 && prod <= 9 ) 
+			else if (num2 != 0 && prod <= 9 )
 
 			{
 				_putchar(',');
@@ -26,7 +26,7 @@ void times_table(void)
 				_putchar(' ');
 				_putchar(prod + '0');
 			}
-			else if (num2 != 0 && prod > 9) 
+			else if (num2 != 0 && prod > 9)
 			{
 				_putchar(',');
 				_putchar(' ');
