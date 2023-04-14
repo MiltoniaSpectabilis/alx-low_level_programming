1bb7c9a34abdcfefa79cee2b5aa02382871486ff
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-04-14 10:59:49 +0000

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,41 +2,39 @@
 
 /**
  * times_table - prints the 9 times table, starting with 0
- *
  */
 
-void times_table(void)
-
+void times_table(void) 
 {
-	int num1, num2, res;
+	int num1, num2, prod;
 
-	for (num1 = 0; num1 <= 9; num1++)
+	for (num1 = 0; num1 <= 9; num1++) 
 	{
-		for (num2 = 0; num2 <= 9; num2++)
+		for (num2 = 0; num2 <= 9; num2++) 
 		{
-			res = num1 * num2;
-			if (res <= 9)
-			{
-				_putchar(res + '0');
-				if (num2 < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
-			}
-			else
-			{
-				_putchar(res / 10 + '0');
-				_putchar(res % 10 + '0');
-				if (num2 < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-			}
+			prod = num1 * num2;
 
+			if (num2 == 0) 
+			{
+				_putchar(prod + '0');
+			}
+			else if (num2 != 0 && prod <= 9 ) 
+
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(prod + '0');
+			}
+			else if (num2 != 0 && prod > 9) 
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(prod / 10 + '0');
+				_putchar(prod % 10 + '0');
+			}
 		}
 		_putchar('\n');
 	}
 }
+
