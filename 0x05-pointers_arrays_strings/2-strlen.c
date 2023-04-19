52ce13665d02544d3dd609e1fd0b7b6b9c2b76a0
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-04-19 16:58:01 +0000

    reset

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,19 +1,21 @@
 #include "main.h"
+
+
 /**
- * _strlen - returns the length of a string
- * @s: string
- * Return: length
+ * _strlen -  returns the length of a string
+ * @*s : string of characters
  */
+
 int _strlen(char *s)
 {
-	int longi = 0;
 
-	while (*s != '\0')
-	{
-		longi++;
-		s++;
-	}
+    int length = 0;
 
-	return (longi);
+    while (*s != '\0') 
+    {
+        length++;
+        s++;
+    }
+    return length;
 }
 
