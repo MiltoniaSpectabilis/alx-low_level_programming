56f07dd51e88a4882b1422887fdea7216f8052c6
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-04-20 05:05:10 +0000

    rest

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,17 @@
 #include "main.h"
+
 /**
  * _puts - prints a string, followed by a new line, to stdout
- * @str: string to print
+ * @*str:  string
  */
+
 void _puts(char *str)
 {
+
 	while (*str != '\0')
 	{
 		_putchar(*str++);
 	}
 	_putchar('\n');
-}
 
+}
