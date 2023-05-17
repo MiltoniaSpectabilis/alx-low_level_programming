921fce6c9af84061b32f2e06eba42b6c5e60c2b6
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-05-17 03:59:09 +0100

    reset

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,47 +2,55 @@
 
 /**
  * print_times_table - prints the n times table, starting with 0
- * @n: number of the times table
+ * @n: the times table to be printed
  */
+
 void print_times_table(int n)
 {
-	int i, j, k;
-
-	if (n >= 0 && n <= 15)
+	if (m < 15 || m > 0)
 	{
-		for (i = 0; i <= n; i++)
+		for (i = 0; i <= m; i++)
 		{
-			for (j = 0; j <= n; j++)
+			for (j = 0; j <= m; j++)
 			{
-				k = j * i;
+				prod = i * j;
+
 				if (j == 0)
 				{
-					_putchar(k + '0');
-				} else if (k < 10 && j != 0)
+					putchar(prod + '0');
+				}
+
+				else if (prod <= 9)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(k + '0');
-				} else if (k >= 10 && k < 100)
+					putchar(',');
+					putchar(' ');
+					putchar(' ');
+					putchar(' ');
+					putchar(prod + '0');
+				}
+
+				else if (prod <= 99)
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar((k / 10) + '0');
-					_putchar((k % 10) + '0');
-				} else if (k >= 100)
+					putchar(',');
+					putchar(' ');
+					putchar(' ');
+					putchar(prod / 10 + '0');
+					putchar(prod % 10 + '0');
+				}
+
+				else
 				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar((k / 100) + '0');
-					_putchar(((k / 10) % 10) + '0');
-					_putchar((k % 10) + '0');
+					putchar(',');
+					putchar(' ');
+					putchar(prod / 100 + '0');
+					putchar((prod / 10) % 10 + '0');
+					putchar(prod % 10 + '0');
 				}
 			}
-			_putchar('\n');
+			putchar('\n');
 		}
+
+
 	}
 }
 
