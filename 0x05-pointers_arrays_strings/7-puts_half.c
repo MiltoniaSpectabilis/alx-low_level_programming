143bb70d11d96d4f8a84635e9176fd1c46d51c5c
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-05-22 12:42:18 +0100

    reset

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,29 +5,29 @@
  * @str: The input string.
  */
 
-void print_second_half(char *str)
+void puts_half(char *str)
 {
-    int len = 0;
+    int len = 0, i;
 
-    // calculate the length of the input string
+    /* calculate the length of the input string */
     while (str[len] != '\0')
     {
         len++;
     }
 
-    // print the second half or last n characters of the string
-    if (len % 2 == 0) // even number of characters
+    /* print the second half or last n characters of the string */
+    if (len % 2 == 0) /* even number of characters */
     {
-        for (int i = len / 2; str[i] != '\0'; i++)
+        for (i = len / 2; str[i] != '\0'; i++)
         {
             putchar(str[i]);
         }
     }
 
-    else // odd number of characters
+    else /* odd number of characters */
     {
         int n = (len - 1) / 2;
-        for (int i = len - n; str[i] != '\0'; i++)
+        for (i = len - n; str[i] != '\0'; i++)
         {
             putchar(str[i]);
         }
