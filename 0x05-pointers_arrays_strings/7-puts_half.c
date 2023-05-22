92de537d36c54a37bcb4ba5d00b1ff65938d74b3
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-05-22 12:43:35 +0100

    reset

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,36 +1,36 @@
 #include "main.h"
 /**
- * print_second_half - Prints the second half or last n characters of a string.
+ * puts_half - Prints the second half or last n characters of a string.
  *
  * @str: The input string.
  */
 
 void puts_half(char *str)
 {
-    int len = 0, i;
+	int len = 0, i;
 
-    /* calculate the length of the input string */
-    while (str[len] != '\0')
-    {
-        len++;
-    }
+	/* calculate the length of the input string */
+	while (str[len] != '\0')
+	{
+		len++;
+	}
 
-    /* print the second half or last n characters of the string */
-    if (len % 2 == 0) /* even number of characters */
-    {
-        for (i = len / 2; str[i] != '\0'; i++)
-        {
-            putchar(str[i]);
-        }
-    }
+	/* print the second half or last n characters of the string */
+	if (len % 2 == 0) /* even number of characters */
+	{
+		for (i = len / 2; str[i] != '\0'; i++)
+		{
+			putchar(str[i]);
+		}
+	}
 
-    else /* odd number of characters */
-    {
-        int n = (len - 1) / 2;
-        for (i = len - n; str[i] != '\0'; i++)
-        {
-            putchar(str[i]);
-        }
-    }
-    putchar('\n');
+	else /* odd number of characters */
+	{
+		int n = (len - 1) / 2;
+		for (i = len - n; str[i] != '\0'; i++)
+		{
+			putchar(str[i]);
+		}
+	}
+	putchar('\n');
 }
