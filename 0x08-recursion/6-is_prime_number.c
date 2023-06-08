2491d7e243564920f97ac242ad1c4bd2121caab0
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-06-08 01:47:10 +0100

    Implement is_prime_number function to check if a number is prime using recursion and a helper function

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -16,10 +16,10 @@ int _is_prime_helper(int n, int i);
 
 int is_prime_number(int n)
 {
-    if (n <= 1)
-        return (0);
+	if (n <= 1)
+		return (0);
 
-    return (_is_prime_helper(n, 2));
+	return (_is_prime_helper(n, 2));
 }
 
 /**
@@ -37,11 +37,11 @@ int is_prime_number(int n)
 
 int _is_prime_helper(int n, int i)
 {
-    if (n == i)
-        return (1);
+	if (n == i)
+		return (1);
 
-    if (n % i == 0)
-        return (0);
+	if (n % i == 0)
+		return (0);
 
-    return (_is_prime_helper(n, i + 1));
+	return (_is_prime_helper(n, i + 1));
 }
