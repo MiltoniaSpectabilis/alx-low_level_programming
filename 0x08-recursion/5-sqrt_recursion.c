a72ba9a29be35d9fcbd292d36d44e05d07038202
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-06-08 01:03:00 +0100

    Implement _sqrt_recursion function to find natural square root of a number using recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -14,7 +14,7 @@ int _sqrt_helper(int n, int i);
 
 int _sqrt_recursion(int n)
 {
-    return _sqrt_helper(n, 1);
+	return _sqrt_helper(n, 1);
 }
 
 /**
@@ -32,11 +32,11 @@ int _sqrt_recursion(int n)
 
 int _sqrt_helper(int n, int i)
 {
-    if (i * i == n)
-        return i;
+	if (i * i == n)
+		return i;
 
-    if (i * i > n)
-        return -1;
+	if (i * i > n)
+		return -1;
 
-    return _sqrt_helper(n, i + 1);
+	return _sqrt_helper(n, i + 1);
 }
