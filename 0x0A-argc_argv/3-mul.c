f39e11a2d508cf9252595d549797e99006274b8f
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-06-11 03:27:49 +0100

    Add code to multiply two numbers passed as command-line arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,7 +11,7 @@
 
 int main(int argc, char *argv[])
 {
-	int num1, num2, prod;
+	int num1, num2;
 
 	if (argc != 3)
 	{
@@ -21,9 +21,7 @@ int main(int argc, char *argv[])
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
-	prod = num1 * num2;
 
-	printf("%d\n", prod);
-
-	return (0);
+	printf("%d",  num1 * num2);
 }
+
