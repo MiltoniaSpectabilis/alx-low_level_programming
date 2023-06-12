da8b026c760f199e750c6f0b46e25287b386030f
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-06-12 23:39:51 +0100

    Add str_concat function for concatenating two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,7 +15,7 @@ char *str_concat(char *s1, char *s2)
 	unsigned int len1 = 0, len2 = 0, i = 0, j = 0;
 	char *str;
 
-	if (s1 == NULL && s2 == NULL)
+	if (s1 == NULL || s2 == NULL)
 		return (NULL);
 
 	while (s1[len1])
