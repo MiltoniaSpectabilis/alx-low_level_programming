42268c49782b36d3d6515ec34a143a0e79dbe1a4
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-06-15 04:06:34 +0100

    Implement _calloc function with proper memory allocation and initialization

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _calloc - Allocates and initializes a memory block.
+ * @nmemb: Number of elements to allocate.
+ * @size: Size of each element.
+ *
+ * Return: Pointer to the allocated and initialized memory block,
+ *         or NULL if allocation fails or either nmemb or size is 0.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	int *arr;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	arr = malloc(nmemb * size);
+	if (arr != NULL)
+		memset(arr, 0, nmemb * size);
+	else
+		return (NULL);
+
+	return (arr);
+}
