1544704ba62f467eb6a946f022dce2217b6cdccd
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-06-19 19:45:46 +0100

    Fix syntax and Betty

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
-typedef struct{
-    char* name;
-    float age;
-    char* owner;
-} dog;
+/**
+ * struct dog - Represents a dog.
+ * @name: The name of the dog.
+ * @age: The age of the dog.
+ * @owner: The owner of the dog.
+ *
+ * Description: Structure that defines a dog with its name, age, and owner.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
 
 #endif /* DOG_H */
