3a7a17495f9080a110481a62a0d0a3d48e1d1c16
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-06-19 23:27:19 +0100

    Fix syntax issues

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -8,9 +8,12 @@
 void free_dog(dog_t *d)
 {
 	if (d)
-		return;
+	{
+		if (d->name)
+			free(d->name);
+		if (d->owner)
+			free(d->owner);
 
-	free(d->name);
-	free(d->owner);
-	free(d);
+		free(d);
+	}
 }
