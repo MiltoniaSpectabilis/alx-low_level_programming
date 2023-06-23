5e684ffa814038a35004ca1fe186ce5ebd8eae53
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-06-23 02:18:47 +0100

    Update code to use switch statement instead of if

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,6 +1,6 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
 #include <stdio.h>
+#include <stdarg.h>
 
 /**
  * print_all - prints anything
@@ -15,45 +15,39 @@
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	unsigned int i = 0;
-	char *sep = "";
-	char c;
-	int n;
-	float f;
-	char *s;
+	int i = 0;
+	char *separator = "";
 
 	va_start(args, format);
 
-	while (format && format[i])
+	while (format && format[i] != '\0')
 	{
-		if (i > 0 && format[i - 1])
-			sep = ", ";
-
-		c = format[i];
-		if (c == 'c')
+		switch (format[i])
 		{
-			printf("%s%c", sep, va_arg(args, int));
-		}
-		else if (c == 'i')
-		{
-			printf("%s%d", sep, va_arg(args, int));
-		}
-		else if (c == 'f')
-		{
-			printf("%s%f", sep, (float)va_arg(args, double));
-		}
-		else if (c == 's')
-		{
-			s = va_arg(args, char *);
-			if (s == NULL)
-				printf("%s(nil)", sep);
-			else
-				printf("%s%s", sep, s);
+			case 'c':
+				printf("%s%c", separator, va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(args, double));
+				break;
+			case 's':
+				separator = (va_arg(args, char *) == NULL) ? "(nil)" : va_arg(args, char *);
+				printf("%s%s", separator);
+				separator = "";
+				break;
+			default:
+				i++;
+				continue;
 		}
 		i++;
+		separator = ", ";
 	}
 
 	printf("\n");
 
 	va_end(args);
 }
+
