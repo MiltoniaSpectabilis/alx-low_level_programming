3533bcb112ee1ee0ea5401222a848f0cd244c423
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-06-23 02:35:32 +0100

    Refactor print_all function for clarity and readability

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -4,50 +4,50 @@
 
 /**
  * print_all - prints anything
- * @format: a list of types of arguments passed to the function
- * c: char
- * i: integer
- * f: float
- * s: char * (if the string is NULL, print (nil) instead
+ * @format: format string containing types of arguments
+ *				to print: c for char, i for int, f for float
+ *				and s for char pointer (string)
  *
- * Return: void
+ * Return: Nothing
  */
 void print_all(const char * const format, ...)
 {
-	va_list args;
 	int i = 0;
-	char *separator = "";
+	char *str, *sep = "";
+
+	va_list args;
 
 	va_start(args, format);
 
-	while (format && format[i] != '\0')
+	if (format)
 	{
-		switch (format[i])
+		while (format[i])
 		{
-			case 'c':
-				printf("%s%c", separator, va_arg(args, int));
-				break;
-			case 'i':
-				printf("%s%d", separator, va_arg(args, int));
-				break;
-			case 'f':
-				printf("%s%f", separator, va_arg(args, double));
-				break;
-			case 's':
-				separator = (va_arg(args, char *) == NULL) ? "(nil)" : va_arg(args, char *);
-				printf("%s%s", separator);
-				separator = "";
-				break;
-			default:
-				i++;
-				continue;
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(args, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(args, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(args, double));
+					break;
+				case 's':
+					str = va_arg(args, char *);
+					str = str ? str : "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
 		}
-		i++;
-		separator = ", ";
 	}
 
 	printf("\n");
-
 	va_end(args);
 }
-
