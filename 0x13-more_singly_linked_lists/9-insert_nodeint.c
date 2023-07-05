3dc372e2f7f94a0064279ee2936e1ff0c79bb540
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-07-05 16:13:01 +0100

    Fix syntax

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,14 +11,12 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
+	listint_t *current = *head;
+	listint_t *new_node = malloc(sizeof(listint_t));
 
 	if (head == NULL)
 		return (NULL);
-
-	listint_t *current = *head;
-
-	listint_t *new_node = malloc(sizeof(listint_t));
-
+	
 	if (new_node == NULL)
 		return (NULL);
 
