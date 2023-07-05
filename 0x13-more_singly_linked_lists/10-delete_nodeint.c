ffc1f621801fa42a6c23487a81e091fd27e9165b
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-07-05 02:09:24 +0100

    Fix syntax

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,6 +10,8 @@
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	unsigned int i;
+
 	if (*head == NULL)
 		return (-1);
 
@@ -23,7 +25,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (1);
 	}
 
-	for (unsigned int i = 0; i < index; i++)
+	for (i = 0; i < index; i++)
 	{
 		prev = current;
 		current = current->next;
