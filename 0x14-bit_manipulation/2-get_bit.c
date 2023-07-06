e84197925e9f3fc152d6b16f873754ff50f60437
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-07-07 00:46:09 +0100

    Betty

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -14,11 +14,11 @@ int get_bit(unsigned long int n, unsigned int index)
 	int bit_value;
 
 	if (index >= sizeof(unsigned long int) * 8)
-		return -1;
+		return (-1);
 
 	mask = 1UL << index;
 	bit_value = (n & mask) != 0;
 
-	return bit_value;
+	return (bit_value);
 }
 
