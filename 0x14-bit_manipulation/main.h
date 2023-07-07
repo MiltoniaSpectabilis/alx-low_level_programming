571b14589ed0c31c83d6bdd9afdcac87f05bd87a
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-07-07 01:07:22 +0100

    Update header file

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdlib.h>
+#include <stdio.h>
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
@@ -11,4 +12,4 @@ int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 
-#endif 
+#endif
