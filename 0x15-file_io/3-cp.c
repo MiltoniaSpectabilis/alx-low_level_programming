3bbff46f30697c7762fb24be0cd93e1a148586ff
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-07-11 02:20:03 +0100

    betty

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,76 +1,72 @@
 #include "main.h"
-#include <errno.h>
-#define BUFFER_SIZE 1024
-
-void error_handling(int file_from, int file_to, char *argv[]);
-void copy_file_contents(int file_from, int file_to, char *argv[]);
 
 /**
- * main - copies the content of a file to another file
- * @argc: the number of arguments passed to the program
- * @argv: an array of strings containing the arguments
- *
- * Return: 0 on success, or an error code on failure
+ * check_file_access - checks if files can be opened.
+ * @source_file: the source file.
+ * @destination_file: the destination file.
+ * @argv: arguments vector.
+ * Return: no return.
  */
-int main(int argc, char *argv[])
+void check_file_access(int source_file, int destination_file, char *argv[])
 {
-	int file_from, file_to;
-
-	if (argc != 3)
+	if (source_file == -1)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
-		exit(97);
-	}
-
-	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_handling(file_from, file_to, argv);
-
-	copy_file_contents(file_from, file_to, argv);
-
-	close(file_from);
-	close(file_to);
-
-	return (0);
-}
-
-/**
- * error_handling - handles errors that may occur during file operations
- * @file_from: the file descriptor of the file being read from
- * @file_to: the file descriptor of the file being written to
- * @argv: an array of strings containing the arguments passed to the program
- */
-void error_handling(int file_from, int file_to, char *argv[])
-{
-	if (file_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Unable to access source file %s\n", argv[1]);
 		exit(98);
 	}
-	if (file_to == -1)
+	if (destination_file == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Unable to access destination file %s\n", argv[2]);
 		exit(99);
 	}
 }
 
 /**
- * copy_file_contents - copies the contents of one file to another
- * @file_from: the file descriptor of the file to read from
- * @file_to: the file descriptor of the file to write to
- * @argv: an array of strings containing the arguments passed to the program
+ * main - program to copy a file.
+ * @argc: number of arguments.
+ * @argv: arguments vector.
+ * Return: Always 0.
  */
-void copy_file_contents(int file_from, int file_to, char *argv[])
+int main(int argc, char *argv[])
 {
+	int source_file, destination_file, err_close;
 	ssize_t nchars, nwr;
-	char buf[BUFFER_SIZE];
+	char buf[1024];
 
-	do {
-		nchars = read(file_from, buf, BUFFER_SIZE);
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp source_file destination_file");
+		exit(97);
+	}
+
+	source_file = open(argv[1], O_RDONLY);
+	destination_file = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC
+			| O_APPEND, 0664);
+	check_file_access(source_file, destination_file, argv);
+
+	nchars = 1024;
+	while (nchars == 1024)
+	{
+		nchars = read(source_file, buf, 1024);
 		if (nchars == -1)
-			error_handling(-1, 0, argv);
-		nwr = write(file_to, buf, nchars);
+			check_file_access(-1, 0, argv);
+		nwr = write(destination_file, buf, nchars);
 		if (nwr == -1)
-			error_handling(0, -1, argv);
-	} while (nchars > 0);
+			check_file_access(0, -1, argv);
+	}
+
+	err_close = close(source_file);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Unable to close source file %d\n", source_file);
+		exit(100);
+	}
+	err_close = close(destination_file);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Unable to close destination file %d\n",
+				destination_file);
+		exit(100);
+	}
+	return (0);
 }
