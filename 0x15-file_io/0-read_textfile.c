d158f2598014059f7f77734979bb025c7acd76ad
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-07-11 01:16:00 +0100

    Code refactoring

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -6,23 +6,31 @@
  * @letters: the number of letters to read and print
  *
  * Return: the actual number of letters that were read and printed, or 0 if an
- *         error occurred
+ * error occurred
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd, num_bytes, num_written;
-	char buffer[1024];
+	char *buffer;
 
 	if (filename == NULL)
 		return (0);
 
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+	{
+		free(buffer);
 		return (0);
+	}
 
 	num_bytes = read(fd, buffer, letters);
 	if (num_bytes == -1)
 	{
+		free(buffer);
 		close(fd);
 		return (0);
 	}
@@ -30,10 +38,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	num_written = write(STDOUT_FILENO, buffer, num_bytes);
 	if (num_written == -1 || num_written != num_bytes)
 	{
+		free(buffer);
 		close(fd);
 		return (0);
 	}
 
+	free(buffer);
 	close(fd);
 
 	return (num_bytes);
