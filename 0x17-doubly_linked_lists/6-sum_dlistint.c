013ac90e6d4b828ec7dd75e875fc3102d9e8485c
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2023-09-14 05:34:47 +0100

    fix doc

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -5,7 +5,7 @@
  * 		  of a dlistint_t linked list.
  * @head: A pointer to the head of the dlistint_t list.
  *
- * Return: The sum of all the data (n) of the dlistint_t list,
+ * Return: The sum of all the data (n) of the dlistint_t list
  * 	   or 0 if it is empty.
  */
 int sum_dlistint(dlistint_t *head)
