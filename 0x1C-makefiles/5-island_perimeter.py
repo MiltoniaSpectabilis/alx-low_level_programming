72b0e642c265b97d1482a8b5508e2e5c58075ea3
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-05-30 02:40:26 +0100

    pep8

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -13,7 +13,7 @@ def island_perimeter(grid):
     Grid is rectangular, width and height don’t exceed 100
     Grid is completely surrounded by water,
     and there is one island (or nothing).
-    The island doesn’t have “lakes” 
+    The island doesn’t have “lakes”
     (water inside that isn’t connected to the water around the island).
 
     :param grid: a list of list of integers
