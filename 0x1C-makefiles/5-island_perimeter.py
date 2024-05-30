0d43415d4348c4f6e7294d65060b4e7457309fe4
MiltoniaSpectabilis <phalaenopsisviridis@gmail.com>
2024-05-30 02:37:32 +0100

    push

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+
+def island_perimeter(grid):
+    """
+    Returns the perimeter of the island described in grid.
+
+    Grid is a list of list of integers:
+    0 represents a water zone
+    1 represents a land zone
+    One cell is a square with side length 1
+    Grid cells are connected horizontally/vertically (not diagonally).
+    Grid is rectangular, width and height don’t exceed 100
+    Grid is completely surrounded by water, and there is one island (or nothing).
+    The island doesn’t have “lakes” (water inside that isn’t connected to the water around the island).
+
+    :param grid: a list of list of integers
+    :return: the perimeter of the island
+    """
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                # Check top edge
+                if i == 0 or grid[i-1][j] == 0:
+                    perimeter += 1
+                # Check bottom edge
+                if i == len(grid) - 1 or grid[i+1][j] == 0:
+                    perimeter += 1
+                # Check left edge
+                if j == 0 or grid[i][j-1] == 0:
+                    perimeter += 1
+                # Check right edge
+                if j == len(grid[i]) - 1 or grid[i][j+1] == 0:
+                    perimeter += 1
+    return perimeter
